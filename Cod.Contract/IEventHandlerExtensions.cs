7deaf1a5cd37b5ed598fca0772e97a43fae77610
W Han <wen.han@outlook.com>
2020-01-05 23:36:49 +1300

    Added extensions method to support invoking of IEventHandlers.

--- Cod.Contract/IEventHandlerExtensions.cs
+++ Cod.Contract/IEventHandlerExtensions.cs
@@ -1,9 +1,18 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Cod
 {
     public static class IEventHandlerExtensions
     {
-        public static async Task HandleAsync<T>(this IEventHandler<T> eventHandler, T sender) => await eventHandler.HandleAsync(sender, null);
+        public static async Task InvokeAsync<T>(this IEventHandler<T> eventHandler, T sender) => await eventHandler.HandleAsync(sender, null);
+
+        public static async Task InvokeAsync<T>(this IEnumerable<IEventHandler<T>> handlers, T sender, object e)
+        {
+            foreach (var eventHandler in handlers)
+            {
+                await eventHandler.HandleAsync(sender, e);
+            }
+        }
     }
 }
