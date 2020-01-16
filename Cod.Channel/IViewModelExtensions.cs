48b3ab55aa96ebfcb4d4c716efc101f68b5b7b72
W Han <wen.han@outlook.com>
2020-01-17 06:29:15 +1300

    Adjusted generic parameter order to make better development experience.

--- Cod.Channel/IViewModelExtensions.cs
+++ Cod.Channel/IViewModelExtensions.cs
@@ -6,7 +6,7 @@ namespace Cod.Channel
 {
     public static class IViewModelExtensions
     {
-        public static IEnumerable<TViewModel> ToViewModel<TDomain, TViewModel, TEntity>(this IEnumerable<TDomain> domains, Func<TViewModel> createViewModel)
+        public static IEnumerable<TViewModel> ToViewModel<TEntity, TViewModel, TDomain>(this IEnumerable<TDomain> domains, Func<TViewModel> createViewModel)
             where TViewModel : IViewModel<TDomain, TEntity>
             where TDomain : IChannelDomain<TEntity>
             => domains.Select(d => (TViewModel)createViewModel().Initialize(d));
