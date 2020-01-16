9238a41e4534cf45cadfc231c35e11021341cfbc
W Han <wen.han@outlook.com>
2020-01-17 07:00:17 +1300

    Improved development experience on ToViewModel method call.

--- Cod.Channel/IViewModelExtensions.cs
+++ Cod.Channel/IViewModelExtensions.cs
@@ -6,7 +6,7 @@ namespace Cod.Channel
 {
     public static class IViewModelExtensions
     {
-        public static IEnumerable<TViewModel> ToViewModel<TEntity, TViewModel, TDomain>(this IEnumerable<TDomain> domains, Func<TViewModel> createViewModel)
+        public static IEnumerable<TViewModel> ToViewModel<TEntity, TViewModel, TDomain>(this IEnumerable<TDomain> domains, Func<TViewModel> createViewModel, TEntity dummy)
             where TViewModel : IViewModel<TDomain, TEntity>
             where TDomain : IChannelDomain<TEntity>
             => domains.Select(d => (TViewModel)createViewModel().Initialize(d));
