4361879e946f49f81c311f49e23e483e62d527f8
W Han <wen.han@outlook.com>
2020-03-13 01:50:11 +1300

    Added helper method to Channel.IRepository.

---
+++ Cod.Channel/IRepositoryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Cod.Channel
+{
+    public static class IRepositoryExtensions
+    {
+        public static async Task<OperationResult<ContinuationToken>> LoadByPrefixAsync<TDomain, TEntity>(
+            this IRepository<TDomain, TEntity> repository,
+            string partitionKeyPrefix,
+            int count = -1,
+            bool force = false)
+            where TDomain : IChannelDomain<TEntity>
+            => await repository.LoadAsync(partitionKeyPrefix, $"{partitionKeyPrefix}z", count, force);
+    }
+}
