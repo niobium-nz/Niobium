7e9a137dcdde10bb0e68f4b5a12941acdfb0d59e
W Han <wen.han@outlook.com>
2020-05-15 01:43:01 +1200

    Added helper methods for role management to user model.

--- Cod.Contract/User.cs
+++ Cod.Contract/User.cs
@@ -26,13 +26,15 @@ namespace Cod
 
         public bool IsBusinessThePartitionKey { get; set; }
 
+        public static string BuildKey(Guid value) => value.ToString("N").ToUpperInvariant();
+
         public Guid GetBusiness()
             => this.IsBusinessThePartitionKey ? Guid.Parse(this.PartitionKey) : Guid.Parse(this.RowKey);
 
         public void SetBusiness(Guid value)
         {
-            if (this.IsBusinessThePartitionKey) this.PartitionKey = value.ToString("N").ToUpperInvariant();
-            else this.RowKey = value.ToString("N").ToUpperInvariant();
+            if (this.IsBusinessThePartitionKey) this.PartitionKey = BuildKey(value);
+            else this.RowKey = BuildKey(value);
         }
 
         public Guid GetID()
@@ -40,11 +42,57 @@ namespace Cod
 
         public void SetID(Guid value)
         {
-            if (this.IsBusinessThePartitionKey) this.RowKey = value.ToString("N").ToUpperInvariant();
-            else this.PartitionKey = value.ToString("N").ToUpperInvariant();
+            if (this.IsBusinessThePartitionKey) this.RowKey = BuildKey(value);
+            else this.PartitionKey = BuildKey(value);
         }
 
         public IEnumerable<string> GetRoles()
-            => this.Roles == null ? Enumerable.Empty<string>() : this.Roles.Split(',');
+            => this.Roles == null ? Enumerable.Empty<string>() : this.Roles.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+        public void AddRole(string role)
+        {
+            if (role is null)
+            {
+                throw new ArgumentNullException(nameof(role));
+            }
+
+            role = role.Trim().ToUpperInvariant();
+            if (String.IsNullOrEmpty(this.Roles))
+            {
+                this.Roles = role;
+            }
+            else
+            {
+                var roles = this.GetRoles().ToList();
+                if (!roles.Contains(role))
+                {
+                    roles.Add(role);
+                    this.Roles = String.Join(",", roles);
+                }
+            }
+        }
+
+        public void RemoveRole(string role)
+        {
+            if (role is null)
+            {
+                throw new ArgumentNullException(nameof(role));
+            }
+
+            role = role.Trim().ToUpperInvariant();
+            if (String.IsNullOrEmpty(this.Roles))
+            {
+                return;
+            }
+            else
+            {
+                var roles = this.GetRoles().ToList();
+                if (roles.Contains(role))
+                {
+                    roles.Remove(role);
+                    this.Roles = String.Join(",", roles);
+                }
+            }
+        }
     }
 }
