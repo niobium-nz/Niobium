425c09369ad251020794cf61cf5a63246f295a28
W Han <wen.han@outlook.com>
2020-05-18 01:06:40 +1200

    Changed entitlement category splitor into string safe to used on storage account.

--- Cod.Contract/Entitlements.cs
+++ Cod.Contract/Entitlements.cs
@@ -5,12 +5,12 @@
 
         public static readonly string[] ScopeSplitor = new[] { ":" };
         public static readonly string[] ValueSplitor = new[] { "," };
-        public static string CategoryNamingPrefix { get; set; }
+        public const string CategoryNamingPrefix = "COD-";
 
         public const string Account = "ACCOUNT";
         public const string AccountManagement = "ACCOUNT_MANAGEMENT";
 
-        public static readonly string NEWAccount = $"{CategoryNamingPrefix}/account";
+        public const string NEWAccount = CategoryNamingPrefix + "ACCOUNT";
         public const string NEWAccountList = "ACCOUNT_LIST";
         public const string NEWAccountManage = "ACCOUNT_MANAGE";
     }
