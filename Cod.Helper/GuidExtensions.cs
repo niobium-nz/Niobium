5ad1988804d6304f3307ff2a47d50279938a4030
Soul <alinalisoul@gmail.com>
2020-06-01 15:53:01 +0800

    Add static method to parse guid

---
+++ Cod.Helper/GuidExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cod
+{
+    public static class GuidExtensions
+    {
+        public static string ToKey(this Guid guid)
+        {
+            return guid.ToString("N").ToUpperInvariant();
+        }
+    }
+}
