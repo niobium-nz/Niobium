8d87bf50741d3c40c128a7cd4f18c602e1e6bfa2
Soul <alinalisoul@gmail.com>
2020-06-06 18:18:16 +0800

    add class ChangeAssignmentParameter

---
+++ Cod.Contract/ChangeAssignmentParameter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Cod
+{
+    public class ChangeAssignmentParameter
+    {
+        public StorageKey Key { get; set; }
+
+        public IEnumerable<StorageKey> Added { get; set; }
+
+        public IEnumerable<StorageKey> Removed { get; set; }
+    }
+}
