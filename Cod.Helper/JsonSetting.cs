6482eb3bb7378015ed96d3c058c44cb07c7ce258
Soul <alinalisoul@gmail.com>
2020-06-06 17:45:11 +0800

    Moved JsonSetting from platform side into shared helper library

---
+++ Cod.Helper/JsonSetting.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Cod
+{
+    public abstract class JsonSetting
+    {
+        public static readonly JsonSerializerSettings CamelCase = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+        public static readonly JsonSerializerSettings UnderstoreCase = new JsonSerializerSettings { ContractResolver = new DefaultContractResolver { NamingStrategy = new SnakeCaseNamingStrategy() } };
+
+        public static readonly JsonSerializerSettings Typed = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto };
+    }
+}
