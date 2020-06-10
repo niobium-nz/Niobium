ebebd061dbef2a717061c2a9bde64be494a30825
Soul <alinalisoul@gmail.com>
2020-06-10 13:06:26 +0800

    move guid extension to Cod project add extension method for storagekey

---
+++ Cod.Contract/IEntityExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Cod
+{
+    public static class IEntityExtensions
+    {
+        public static StorageKey GetKey(this IEntity entity)
+        {
+            return new StorageKey { PartitionKey = entity.PartitionKey, RowKey = entity.RowKey };
+        }
+    }
+}
---
+++ Cod.Contract/StorageKeyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cod
+{
+    public static class StorageKeyExtensions
+    {
+        public static string BuildFullID(this StorageKey key)
+        {
+            return $"{key.PartitionKey}-{key.RowKey}";
+        }
+
+        public static StorageKey ParseFullID(string fullID)
+        {
+            var splited = fullID.Split(new string[] { "-" }, System.StringSplitOptions.RemoveEmptyEntries);
+            if (splited.Length == 2)
+            {
+                return new StorageKey { PartitionKey = splited[0], RowKey = splited[1] };
+            }
+            throw new NotSupportedException();
+        }
+    }
+}
