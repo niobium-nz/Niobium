c79e71ab3a7bed5249293edb151b8dbc854d423a
W Han <wen.han@outlook.com>
2020-06-12 13:39:25 +1200

    Bug fix on view model refresh helper where non-existing entries are not removed.

--- Cod.Channel/IViewModelExtensions.cs
+++ Cod.Channel/IViewModelExtensions.cs
@@ -33,10 +33,23 @@ namespace Cod.Channel
                 var added = !existings.Any(e =>
                     e.PartitionKey == refreshment.PartitionKey
                     && e.RowKey == refreshment.RowKey);
+
                 if (added)
                 {
                     var vm = (TViewModel)createViewModel().Initialize(refreshment);
                     existings.Add(vm);
+                }
+            }
+
+            for (var i = existings.Count - 1; i >= 0; i--)
+            {
+                var removed = !refreshments.Any(e =>
+                    e.PartitionKey == existings[i].PartitionKey
+                    && e.RowKey == existings[i].RowKey);
+
+                if (removed)
+                {
+                    existings.Remove(existings[i]);
                 }
             }
 
