aafba83e9b504d8258cb445d6ba5962db7b96613
Soul <alinalisoul@gmail.com>
2020-07-17 10:13:21 +0800

    add method to format file size

---
+++ Cod.Helper/FileSizeFormater.cs
@@ -0,0 +1,29 @@
+﻿namespace Cod
+{
+    public static class FileSizeFormater
+    {
+        public static string FormatFileSize(this long size)
+        {
+            var result = size / 1d;
+            if (result < 1024)
+            {
+                return $"{result} B";
+            }
+
+            result /= 1024d;
+            if (result<1024)
+            {
+                return $"{result} Kb";
+            }
+
+            result /= 1024d;
+            if (result < 1024)
+            {
+                return $"{result} Mb";
+            }
+
+            result /= 1024d;
+            return $"{result} Gb";
+        }
+    }
+}
