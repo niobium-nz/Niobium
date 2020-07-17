3a77f36abba6ac0430ef39646d242daf6e3d43ad
Soul <alinalisoul@gmail.com>
2020-07-17 18:00:32 +0800

    add method to format file size

--- Cod.Helper/FileSizeFormater.cs
+++ Cod.Helper/FileSizeFormater.cs
@@ -13,17 +13,17 @@
             result /= 1024d;
             if (result<1024)
             {
-                return $"{result} Kb";
+                return $"{result} KB";
             }
 
             result /= 1024d;
             if (result < 1024)
             {
-                return $"{result} Mb";
+                return $"{result} MB";
             }
 
             result /= 1024d;
-            return $"{result} Gb";
+            return $"{result} GB";
         }
     }
 }
