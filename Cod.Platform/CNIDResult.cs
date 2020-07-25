b91f1b51d7214f6fddc2c7f4c3bb60ea950fdc28
wangzhiheng <wangzhiheng@tigerjoys.com>
2020-07-25 20:01:45 +0800

    增加验证

--- Cod.Platform/CNIDResult.cs
+++ Cod.Platform/CNIDResult.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Cod.Platform
 {
     public class CNIDResult
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Gender { get; set; }
+        [Required]
         public string Nationality { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "生日错误")]
         public string Birthday { get; set; }
+        [Required]
+        [StringLength(18, MinimumLength = 18, ErrorMessage = "身份证号码错误")]
         public string Number { get; set; }
     }
 }
