a146bf580cff0b1cce5557a0e26da2d0146ebc40
wangzhiheng <wangzhiheng@tigerjoys.com>
2020-07-25 19:55:14 +0800

    增加 身份证识别 Response

---
+++ Cod.Platform/CNIDResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cod.Platform
+{
+    public class CNIDResponse
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Nationality { get; set; }
+        public string Address { get; set; }
+        public string Birthday { get; set; }
+        public string Number { get; set; }
+    }
+}
