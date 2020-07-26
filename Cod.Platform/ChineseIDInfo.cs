1950961422ab53df2183b77328498c6324d9d802
wangzhiheng <wangzhiheng@tigerjoys.com>
2020-07-26 17:37:33 +0800

    重命名

--- Cod.Platform/ChineseIDInfo.cs
+++ Cod.Platform/ChineseIDInfo.cs
@@ -5,19 +5,18 @@ using System.Text;
 
 namespace Cod.Platform
 {
-    public class CNIDResult
+    public class ChineseIDInfo
     {
         [Required]
         public string Name { get; set; }
         [Required]
-        public string Gender { get; set; }
+        public Gender Gender { get; set; }
         [Required]
-        public string Nationality { get; set; }
+        public string Race { get; set; }
         [Required]
         public string Address { get; set; }
         [Required]
-        [StringLength(8, MinimumLength = 8, ErrorMessage = "生日错误")]
-        public string Birthday { get; set; }
+        public DateTimeOffset Birthday { get; set; }
         [Required]
         [StringLength(18, MinimumLength = 18, ErrorMessage = "身份证号码错误")]
         public string Number { get; set; }
