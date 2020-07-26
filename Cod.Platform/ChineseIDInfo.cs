5e9251a0a5c8f2d354f4160af549fc1c447570b1
wangzhiheng <wangzhiheng@tigerjoys.com>
2020-07-26 17:54:05 +0800

    增加 签发机关 和 有效期 字段

--- Cod.Platform/ChineseIDInfo.cs
+++ Cod.Platform/ChineseIDInfo.cs
@@ -9,16 +9,27 @@ namespace Cod.Platform
     {
         [Required]
         public string Name { get; set; }
+
         [Required]
         public Gender Gender { get; set; }
+
         [Required]
         public string Race { get; set; }
+
         [Required]
         public string Address { get; set; }
+
         [Required]
         public DateTimeOffset Birthday { get; set; }
+
         [Required]
         [StringLength(18, MinimumLength = 18, ErrorMessage = "身份证号码错误")]
         public string Number { get; set; }
+
+        [Required]
+        public string Issue { get; set; }
+
+        [Required]
+        public DateTimeOffset Expiry { get; set; }
     }
 }
