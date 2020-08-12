5fce3ad45c74e8fce19050db9cb1e7d26c9bed0f
W Han <wen.han@outlook.com>
2020-08-12 20:27:02 +1200

    Enhanced generic type converter.

--- Cod.Helper/TypeConverter.cs
+++ Cod.Helper/TypeConverter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using Newtonsoft.Json;
@@ -8,19 +10,37 @@ namespace Cod
     public static class TypeConverter
     {
         private static readonly Type[] ConvertibleTypes = new[] { typeof(Guid), typeof(DateTime), typeof(DateTimeOffset), typeof(TimeSpan), };
+        
+        public static T Convert<T>(string input) => (T)Convert(input, typeof(T));
 
-        public static T Convert<T>(string input)
+        public static object Convert(string input, Type targetType)
         {
-            T obj;
-            var t = typeof(T);
-            if (typeof(IConvertible).IsAssignableFrom(t) || ConvertibleTypes.Contains(t))
+            object obj;
+            if (typeof(IEnumerable).IsAssignableFrom(targetType))
             {
-                var converter = TypeDescriptor.GetConverter(t);
-                obj = (T)converter.ConvertFrom(input);
+                var genericArgs = targetType.GetGenericArguments();
+                if (genericArgs.Length != 1)
+                {
+                    throw new NotSupportedException();
+                }
+
+                var parts = input.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                var result = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(genericArgs));
+                foreach (var part in parts)
+                {
+                    obj = Convert(part, genericArgs[0]);
+                    result.Add(obj);
+                }
+                return result;
+            }
+            else if (typeof(IConvertible).IsAssignableFrom(targetType) || ConvertibleTypes.Contains(targetType))
+            {
+                var converter = TypeDescriptor.GetConverter(targetType);
+                obj = converter.ConvertFrom(input);
             }
             else
             {
-                obj = JsonConvert.DeserializeObject<T>(input);
+                obj = JsonConvert.DeserializeObject(input, targetType);
             }
 
             return obj;
