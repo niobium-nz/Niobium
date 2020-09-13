4a3971d7365e3f1639efe8938088c0f2b1e01f5b
W Han <wen.han@outlook.com>
2020-09-14 02:20:11 +1200

    Expose the default UICulture setting to public to support easier customization.

--- Cod.Abstractions/UICulture.cs
+++ Cod.Abstractions/UICulture.cs
@@ -5,13 +5,11 @@ namespace Cod
 {
     public static class UICulture
     {
-        private static CultureInfo defaultCulture = new CultureInfo("en-US");
+        public static CultureInfo Default = new CultureInfo("en-US");
         private static readonly AsyncLocal<CultureInfo> culture = new AsyncLocal<CultureInfo>();
-
-        public static void Initialize(CultureInfo value) => defaultCulture = value;
 
         public static void Register(CultureInfo value) => culture.Value = value;
 
-        public static CultureInfo Current => culture.Value ?? defaultCulture;
+        public static CultureInfo Current => culture.Value ?? Default;
     }
 }
--- Cod.Platform/Notification/NotificationService.cs
+++ Cod.Platform/Notification/NotificationService.cs
@@ -28,6 +28,7 @@ namespace Cod.Platform
             foreach (var channel in this.channels.Value)
             {
                 var result = await channel.SendAsync(brand, user, context, template, parameters, level);
+                await this.PostSendAsync(result, brand, user, context, template, parameters, level);
                 if (result.IsSuccess)
                 {
                     return new OperationResult<int>(result) { Result = level };
@@ -43,5 +44,8 @@ namespace Cod.Platform
             }
             return await this.SendAsync(brand, user, context, template, parameters, ++level);
         }
+
+        protected virtual Task PostSendAsync(OperationResult result, string brand, Guid user, NotificationContext context, int template, IReadOnlyDictionary<string, object> parameters, int level)
+            => Task.CompletedTask;
     }
 }
