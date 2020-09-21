268622a0ec45af333841bce580e2fd7083917399
W Han <wen.han@outlook.com>
2020-09-21 19:17:30 +1200

    Fixed bug that AuthenticatorInitializer isn't registered correctly with DI.

--- Cod.Channel/DependencyModule.cs
+++ Cod.Channel/DependencyModule.cs
@@ -8,7 +8,7 @@ namespace Cod.Channel
         public void Load(IServiceCollection services)
         {
             InternalError.Register(new InternalErrorRetriever());
-            services.AddTransient<AuthenticatorInitializer>();
+            services.AddTransient<IBootstrapper, AuthenticatorInitializer>();
             services.AddTransient<IQueue, ChannelQueue>();
             services.AddTransient<LoginCommand>();
             services.AddTransient<ICommand>(sp => sp.GetService<LoginCommand>());
