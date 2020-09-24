f2eefd517dde4ca5ee11cdc157fe76504982da0e
W Han <wen.han@outlook.com>
2020-09-25 11:35:08 +1200

    Added helper to compute MD5 hash in base64 format.

---
+++ Cod.Helper/MD5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Cod
+{
+    public static class MD5Helper
+    {
+        public static string ComputeHashAsBase64String(byte[] input)
+        {
+            using var md5 = MD5.Create();
+            var bs = md5.ComputeHash(input);
+            return Convert.ToBase64String(bs);
+        }
+    }
+}
