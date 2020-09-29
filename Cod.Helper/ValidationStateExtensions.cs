52be4556c6515dbc3d22908fdd0b0128e767dbc6
willsun <dotlucene@gmail.com>
2020-09-29 13:03:48 +0800

    修正返回值错误

--- Cod.Helper/ValidationStateExtensions.cs
+++ Cod.Helper/ValidationStateExtensions.cs
@@ -5,7 +5,7 @@
         public static OperationResult ToOperationResult(this ValidationState validationState)
             => new OperationResult(InternalError.BadRequest) { Reference = validationState.ToDictionary() };
 
-        public static OperationResult ToOperationResult<T>(this ValidationState validationState)
+        public static OperationResult<T> ToOperationResult<T>(this ValidationState validationState)
             => new OperationResult<T>(InternalError.BadRequest) { Reference = validationState.ToDictionary() };
     }
 }
