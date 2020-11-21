0c869dd886afa3da1bd94eb2364e52301af6d5f6
willsun <dotlucene@gmail.com>
2020-11-21 11:06:42 +0800

    add  method to DateTimeOffsetExtension for display local add CurrencyExtensions for display local

---
+++ Cod.Abstractions/CurrencyExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Cod
+{
+    public static class CurrencyExtensions
+    {
+        public static string ToDisplayLocal(this float payment, Currency currency)
+        {
+            return ToDisplayLocal((decimal)payment, currency);
+        }
+
+        public static string ToDisplayLocal(this double payment, Currency currency)
+        {
+            return ToDisplayLocal((decimal)payment, currency);
+        }
+
+        public static string ToDisplayLocal(this int payment, Currency currency)
+        {
+            return ToDisplayLocal((decimal)payment, currency);
+        }
+
+        public static string ToDisplayLocal(this decimal payment, Currency currency)
+        {
+            System.Globalization.CultureInfo culture = Currency.GetCulture(currency.Code);
+            return $"{payment.ToString("C2", culture)} {currency.Code}";
+        }
+    }
+}
