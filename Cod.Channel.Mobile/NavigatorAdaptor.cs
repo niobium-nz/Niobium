327bf956b2560c45f2fae6f76c833f2573abf19b
W Han <wen.han@outlook.com>
2021-03-01 19:03:49 +1300

    Bug fix.

--- Cod.Channel.Mobile/NavigatorAdaptor.cs
+++ Cod.Channel.Mobile/NavigatorAdaptor.cs
@@ -8,7 +8,7 @@ namespace Cod.Channel.Mobile
     {
         public string BaseUri => String.Empty;
 
-        public string CurrentUri => Shell.Current.CurrentState.Location.AbsoluteUri;
+        public string CurrentUri => Shell.Current.CurrentState.Location.ToString();
 
         public async Task NavigateToAsync(string url, bool forceLoad = false)
         {
