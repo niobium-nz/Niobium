d9c327541651f28e7a990be8d0a3e6eb6e99046e
Soul <alinalisoul@gmail.com>
2021-09-25 15:18:34 +0800

    fix setting name

--- Cod.Platform/Constant.cs
+++ Cod.Platform/Constant.cs
@@ -2,11 +2,11 @@ namespace Cod.Platform
 {
     public abstract class Constant : Cod.Constant
     {
-        public const string STORAGE_CONNECTION_NAME = "STORAGE-ACCOUNT";
-        public const string HTTP_PROXY_HOST = "PROXY-HOST";
-        public const string HTTP_PROXY_USERNAME = "PROXY-USERNAME";
-        public const string HTTP_PROXY_PASSWORD = "PROXY-PASSWORD";
-        public const string AUTH_SECRET_NAME = "AUTH-TOKEN-KEY";
+        public const string STORAGE_CONNECTION_NAME = "STORAGE_ACCOUNT";
+        public const string HTTP_PROXY_HOST = "PROXY_HOST";
+        public const string HTTP_PROXY_USERNAME = "PROXY_USERNAME";
+        public const string HTTP_PROXY_PASSWORD = "PROXY_PASSWORD";
+        public const string AUTH_SECRET_NAME = "AUTH_TOKEN_KEY";
         public const string API_URL = "API_URL";
 
         public const string NEW_STORAGE_CONNECTION_NAME = "STORAGE_ACCOUNT";
