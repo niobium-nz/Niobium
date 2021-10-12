d709b76dc60e0bbd21a828214da4c27ab85b3076
Wen Han <hcp5he11@gmail.com>
2021-10-12 23:22:06 +1300

    Fixed bug on MemoryCachedRepository where cache are always rebuilt.

--- Cod.Platform/MemoryCachedRepository.cs
+++ Cod.Platform/MemoryCachedRepository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -75,6 +75,7 @@ namespace Cod.Platform
                 var templates = await this.repository.GetAsync();
                 this.Cache.Clear();
                 this.Cache.AddRange(templates);
+                this.lastCached = DateTimeOffset.UtcNow;
             }
         }
     }
