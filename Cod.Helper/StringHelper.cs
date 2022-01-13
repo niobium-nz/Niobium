fe8529cf4889fbc8b89ffbd9a2b794b0d70386f9
Wen Han <hcp5he11@gmail.com>
2022-01-13 22:59:43 +1300

    Added helper to support getting of initials of Chinese characters.

---
+++ Cod.Helper/StringHelper.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Text;
+
+namespace Cod
+{
+    public static class StringHelper
+    {
+        private static readonly Encoding encoding = Encoding.GetEncoding(936);
+        private static readonly int[] areacode = { 45217, 45253, 45761, 46318, 46826, 47010, 47297, 47614, 48119, 48119, 49062, 49324, 49896, 50371, 50614, 50622, 50906, 51387, 51446, 52218, 52698, 52698, 52698, 52980, 53689, 54481 };
+
+        public static string GetChineseInitial(string cnChar)
+        {
+            if (cnChar.Length != 1)
+            {
+                throw new NotSupportedException();
+            }
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            byte[] arrCN = encoding.GetBytes(cnChar);
+            if (arrCN.Length > 1)
+            {
+                int area = (short)arrCN[0];
+                int pos = (short)arrCN[1];
+                int code = (area << 8) + pos;
+
+                for (int i = 0; i < 26; i++)
+                {
+                    int max = 55290;
+
+                    if (i != 25)
+                    {
+                        max = areacode[i + 1];
+                    }
+
+                    if (areacode[i] <= code && code < max)
+                    {
+                        return encoding.GetString(new byte[] { (byte)(65 + i) });
+                    }
+                }
+
+                return "*";
+            }
+            else
+            {
+                return cnChar;
+            }
+        }
+    }
+}
