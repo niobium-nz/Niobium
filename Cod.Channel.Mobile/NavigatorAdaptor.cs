cf502a5073b88b5663c9838a1501f7f143127875
Wen Han <hcp5he11@gmail.com>
2022-01-15 02:43:28 +1300

    Makes .NET6 compiler happy.

--- Cod.Channel.Mobile/NavigatorAdaptor.cs
+++ Cod.Channel.Mobile/NavigatorAdaptor.cs
@@ -12,12 +12,12 @@ namespace Cod.Channel.Mobile
 
         public async Task NavigateToAsync(string url, bool forceLoad = false)
         {
-            if (forceLoad && !url.StartsWith("//"))
+            if (forceLoad && !url.StartsWith("//", StringComparison.InvariantCulture))
             {
                 url = $"//{url}";
             }
 
-            await Shell.Current.GoToAsync(url);
+            await Shell.Current.GoToAsync(url).ConfigureAwait(false);
         }
     }
 }
