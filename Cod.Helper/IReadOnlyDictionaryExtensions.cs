ca6cbb25114e09d1e0cd44e1d5863474e5f9585d
Wen Han <hcp5he11@gmail.com>
2022-03-09 01:26:16 +1300

    Added more extension methods to IReadOnlyDictionary.

--- Cod.Helper/IReadOnlyDictionaryExtensions.cs
+++ Cod.Helper/IReadOnlyDictionaryExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Cod
@@ -26,6 +26,38 @@ namespace Cod
         public static string GetValueAsString(this IReadOnlyDictionary<string, object> dic, string key)
         {
             if (!TryGetValueAsString(dic, key, out var v))
+            {
+                throw new InvalidCastException();
+            }
+            return v;
+        }
+
+        public static bool TryGetValueAsBoolean(this IReadOnlyDictionary<string, object> dic, string key, out bool value)
+        {
+            value = false;
+
+            if (!dic.ContainsKey(key))
+            {
+                return false;
+            }
+            var v = dic[key];
+            if (v is bool v2)
+            {
+                value = v2;
+                return true;
+            }
+            else if (v is string v3)
+            {
+                value = Boolean.Parse(v3);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool GetValueAsBoolean(this IReadOnlyDictionary<string, object> dic, string key)
+        {
+            if (!TryGetValueAsBoolean(dic, key, out var v))
             {
                 throw new InvalidCastException();
             }
