7fbd71a8b7c0090fbdde66a69ccf65ad8bc90864
Soul <alinalisoul@gmail.com>
2022-05-21 18:07:18 +0800

    增加依赖项注册

--- Cod.Channel/DependencyModule.cs
+++ Cod.Channel/DependencyModule.cs
@@ -32,6 +32,7 @@ namespace Cod.Channel
             });
             services.AddTransient<IBootstrapper, AuthenticatorInitializer>();
             services.AddTransient<IQueue, ChannelQueue>();
+            services.AddTransient<IBlob, ChannelBlob>();
             services.AddTransient<LoginCommand>();
             services.AddTransient<ICommand>(sp => sp.GetService<LoginCommand>());
             services.AddTransient<ICommand<LoginCommandParameter>>(sp => sp.GetService<LoginCommand>());
