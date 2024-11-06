caaf529bef3e84b5505969b0b7d7d2347a3b190a
Wen Han <hcp5he11@gmail.com>
2024-11-06 23:04:47 +1300

    Minor refactoring.

--- Cod.Platform.ServiceBus/PrincipalParserExtensions.cs
+++ Cod.Platform.ServiceBus/PrincipalParserExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Cod.Platform.Identity;
 using Microsoft.Net.Http.Headers;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 
 namespace Cod.Messaging.ServiceBus
@@ -10,17 +11,12 @@ namespace Cod.Messaging.ServiceBus
         public static async Task<ClaimsPrincipal?> ParseAsync(this PrincipalParser helper, ServiceBusReceivedMessage message, CancellationToken cancellationToken = default)
         {
             if (message.ApplicationProperties.TryGetValue(HeaderNames.Authorization, out var authorization)
-                && authorization != null && authorization is string auth)
+                && authorization != null && authorization is string auth
+                && AuthenticationHeaderValue.TryParse(auth, out var authHeader))
             {
-                var parts = auth.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                if (parts.Length >= 2)
+                if (authHeader.Scheme == AuthenticationScheme.BearerLoginScheme && !string.IsNullOrWhiteSpace(authHeader.Parameter))
                 {
-                    var scheme = parts[0];
-                    if (scheme == AuthenticationScheme.BearerLoginScheme)
-                    {
-                        var token = auth[(scheme.Length + 1)..];
-                        return await helper.ParseAsync(token, cancellationToken);
-                    }
+                    return await helper.ParseAsync(authHeader.Parameter, cancellationToken);
                 }
             }
 
---
+++ Cod.Platform.ServiceBus/ServiceBusReceivedMessageExtensions.cs
@@ -0,0 +1,72 @@
+﻿using Azure.Messaging.ServiceBus;
+using Cod.Platform.Identity;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Cod.Messaging.ServiceBus
+{
+    public static class ServiceBusReceivedMessageExtensions
+    {
+        private const string JsonMediaType = "application/json";
+        private static readonly JsonSerializerOptions SerializationOptions = new(JsonSerializerDefaults.Web);
+
+        public static bool TryParse<T>(this ServiceBusReceivedMessage message, [NotNullWhen(true)] out T? request, [NotNullWhen(true)] out string? rawbody)
+        {
+            request = default;
+            rawbody = default;
+
+            if (message.ContentType != JsonMediaType)
+            {
+                return false;
+            }
+
+            rawbody = message.Body.ToString();
+            if (string.IsNullOrWhiteSpace(rawbody))
+            {
+                return false;
+            }
+
+            request = Deserialize<T>(rawbody);
+            if (request == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static async Task<Guid> TryGetUserIDAsync(this ServiceBusReceivedMessage message, PrincipalParser principalParser, CancellationToken cancellationToken = default)
+        {
+            var claim = await message.TryGetClaimAsync<string>(principalParser, ClaimTypes.Sid, cancellationToken);
+            if (claim == null || !Guid.TryParse(claim, out var userID))
+            {
+                throw new ApplicationException(InternalError.Forbidden);
+            }
+
+            return userID;
+        }
+
+        public static async Task<T?> TryGetClaimAsync<T>(this ServiceBusReceivedMessage message, PrincipalParser principalParser, string key, CancellationToken cancellationToken = default)
+        {
+            var claims = await message.TryGetClaimsAsync<T>(principalParser, key, cancellationToken);
+            return claims.SingleOrDefault();
+        }
+
+        public static async Task<IEnumerable<T>> TryGetClaimsAsync<T>(this ServiceBusReceivedMessage message, PrincipalParser principalParser, string key, CancellationToken cancellationToken = default)
+        {
+            var principal = await principalParser.ParseAsync(message, cancellationToken: cancellationToken)
+                ?? throw new ApplicationException(InternalError.AuthenticationRequired);
+
+            var claims = principal.Claims.Where(c => c.Type == key);
+            if (!claims.Any())
+            {
+                return [];
+            }
+
+            return claims.Select(c => TypeConverter.Convert<T>(c.Value));
+        }
+
+        private static T Deserialize<T>(string json) => System.Text.Json.JsonSerializer.Deserialize<T>(json, SerializationOptions)!;
+    }
+}
