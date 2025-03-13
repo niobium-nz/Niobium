c0a7c8910e45740d28d74279edc8b0553177dcf2
Wen Han <hcp5he11@gmail.com>
2025-03-13 22:37:26 +1300

    Added extension method for easier domain registration.

---
+++ Cod.Abstractions/IServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Cod
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterDomain<TDomain, TEntity>(this IServiceCollection services) where TDomain : class, IDomain<TEntity>
+        {
+            services.AddTransient<TDomain>();
+            services.AddTransient<Func<TDomain>>(sp => () => sp.GetRequiredService<TDomain>());
+            services.AddTransient<IDomainRepository<TDomain, TEntity>, GenericDomainRepository<TDomain, TEntity>>();
+            return services;
+        }
+    }
+}
