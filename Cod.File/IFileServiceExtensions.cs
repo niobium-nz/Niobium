e9320c59fe4975a27523f9a18572b26366bc69af
Wen Han <hcp5he11@gmail.com>
2025-05-04 23:38:39 +1200

    Added extention method for FileService.

--- Cod.File/IFileServiceExtensions.cs
+++ Cod.File/IFileServiceExtensions.cs
@@ -4,6 +4,21 @@ namespace Cod.File
 {
     public static class IFileServiceExtensions
     {
+        public async static Task<string?> GetAsStringAsync(this IFileService fileService, string partition, string filename, bool createIfNotExist = true, CancellationToken cancellationToken = default)
+        {
+            var stream = await fileService.GetAsync(partition, filename, createIfNotExist, cancellationToken);
+            if (stream == null)
+            {
+                return null;
+            }
+
+            using (stream)
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync(cancellationToken);
+            }
+        }
+
         public async static Task PutAsync(this IFileService fileService, string partition, string filename, string content, bool replaceIfExist = false, IDictionary<string, string>? tags = null, bool createIfNotExist = true, CancellationToken cancellationToken = default)
             => await fileService.PutAsync(partition, filename, Encoding.UTF8.GetBytes(content), replaceIfExist, tags, createIfNotExist, cancellationToken);
 
