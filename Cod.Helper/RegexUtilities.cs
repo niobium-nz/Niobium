208e45539925a340b47e3042b1e2c93d96c02daa
Wen Han <hcp5he11@gmail.com>
2025-05-12 01:37:34 +1200

    Bugfix on email validation.

--- Cod.Helper/RegexUtilities.cs
+++ Cod.Helper/RegexUtilities.cs
@@ -41,8 +41,9 @@ namespace Cod
 
             try
             {
+                // Updated regex to allow underscores at the end of the local part
                 return Regex.IsMatch(email,
-                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z_]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z_])@))" +
                     @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
                     RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
             }
