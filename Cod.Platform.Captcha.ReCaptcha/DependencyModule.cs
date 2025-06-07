479ddcd1f76d0077dba053cfaac6474d2d94026a
Wen Han <hcp5he11@gmail.com>
2025-06-07 18:46:41 +1200

    Added development support on ReCaptcha integration.

--- Cod.Platform.Captcha.ReCaptcha/DependencyModule.cs
+++ Cod.Platform.Captcha.ReCaptcha/DependencyModule.cs
@@ -1,4 +1,5 @@
 using Cod.Platform.Captcha.Recaptcha;
+using Cod.Platform.Captcha.ReCaptcha;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,11 +12,12 @@ namespace Cod.Platform.Captcha
 
         public static IServiceCollection AddCaptcha(this IServiceCollection services, IConfiguration configuration)
         {
-            return services.AddCaptcha(configuration.Bind);
+            return services.AddCaptcha(configuration, configuration.Bind);
         }
 
         public static IServiceCollection AddCaptcha(
             this IServiceCollection services,
+            IConfiguration configuration,
             Action<CaptchaOptions> options)
         {
             if (loaded)
@@ -27,14 +29,22 @@ namespace Cod.Platform.Captcha
 
             services.AddPlatform();
 
-            services.Configure<CaptchaOptions>(o => options(o));
-
-            services.AddTransient<IVisitorRiskAssessor, GoogleReCaptchaRiskAssessor>();
-            services.AddHttpClient<IVisitorRiskAssessor, GoogleReCaptchaRiskAssessor>((sp, httpClient) =>
+            if (configuration.IsDevelopmentEnvironment())
             {
-                httpClient.BaseAddress = new Uri(recaptchaHost);
-            })
-            .AddStandardResilienceHandler();
+                services.AddTransient<IVisitorRiskAssessor, DevelopmentRiskAccessor>();
+            }
+            else
+            {
+                services.Configure<CaptchaOptions>(o => options(o));
+
+                services.AddTransient<IVisitorRiskAssessor, GoogleReCaptchaRiskAssessor>();
+                services.AddHttpClient<IVisitorRiskAssessor, GoogleReCaptchaRiskAssessor>((sp, httpClient) =>
+                {
+                    httpClient.BaseAddress = new Uri(recaptchaHost);
+                })
+                .AddStandardResilienceHandler();
+            }
+                
             return services;
         }
     }
---
+++ Cod.Platform.Captcha.ReCaptcha/DevelopmentRiskAccessor.cs
@@ -0,0 +1,9 @@
+﻿using Cod.Platform.Captcha.Recaptcha;
+
+namespace Cod.Platform.Captcha.ReCaptcha
+{
+    internal class DevelopmentRiskAccessor : IVisitorRiskAssessor
+    {
+        public Task<bool> AssessAsync(Guid requestID, string tenant, string token, string? remoteIP, CancellationToken cancellationToken) => Task.FromResult(true);
+    }
+}
