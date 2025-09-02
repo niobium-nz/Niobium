9e7d605077612e8032a7fe75c88f792584ec3cf4
Wen Han <hcp5he11@gmail.com>
2025-09-03 09:11:20 +1200

    Minor enhancement.

--- Niobium.Platform.Profile/ServicePrincipalProfileService.cs
+++ Niobium.Platform.Profile/ServicePrincipalProfileService.cs
@@ -11,12 +11,12 @@ namespace Niobium.Platform.Profile
         IHttpClientFactory httpClientFactory,
         IOptions<ProfileOptions> options,
         ILogger<GenericProfileService<T>> logger)
-            : GenericProfileService<T>(httpClientFactory, options, logger), IProfileService<T>, IDisposable
+            : GenericProfileService<T>(httpClientFactory, options, logger), IProfileService<T>
                 where T : class, IProfile
     {
-        private const int ClockSkewMinutes = -10;
-        private readonly SemaphoreSlim tokenLock = new(1, 1);
-        private AccessToken? token;
+        private static readonly TimeSpan clockSkew = TimeSpan.FromMinutes(10);
+        private static readonly SemaphoreSlim tokenLock = new(1, 1);
+        private static AccessToken? token;
 
         protected override async Task<HttpClient?> ConfigureHttpClientAsync(HttpClient httpClient, CancellationToken cancellationToken)
         {
@@ -25,19 +25,18 @@ namespace Niobium.Platform.Profile
             return httpClient;
         }
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
         private async Task<string> GetAccessTokenAsync(CancellationToken cancellationToken)
         {
             DateTimeOffset now = DateTimeOffset.UtcNow;
-            if (!token.HasValue || token.Value.ExpiresOn <= now.AddMinutes(ClockSkewMinutes))
+            if (token.HasValue && token.Value.ExpiresOn - now > clockSkew)
             {
-                await tokenLock.WaitAsync(cancellationToken);
-                if (!token.HasValue || token.Value.ExpiresOn <= now.AddMinutes(ClockSkewMinutes))
+                return token.Value.Token;
+            }
+
+            await tokenLock.WaitAsync(cancellationToken);
+            try
+            {
+                if (!token.HasValue || token.Value.ExpiresOn - now < clockSkew)
                 {
                     if (string.IsNullOrWhiteSpace(Options.Value.ProfileAppID))
                     {
@@ -48,16 +47,12 @@ namespace Niobium.Platform.Profile
                     token = await credential.GetTokenAsync(new TokenRequestContext([$"api://{Options.Value.ProfileAppID}/.default"]), cancellationToken);
                 }
             }
+            finally
+            {
+                tokenLock.Release();
+            }
 
             return token.Value.Token;
-        }
-
-        private void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                tokenLock.Dispose();
-            }
         }
     }
 }
