7bd1ba401304d5da9dfcae1165f5bbf84f2d728e
Wen Han <hcp5he11@gmail.com>
2025-10-18 01:03:00 +1300

    Added custom snake case helper.

--- Niobium.Messaging.ServiceBus/StringHelper.cs
+++ Niobium.Messaging.ServiceBus/StringHelper.cs
@@ -28,5 +28,37 @@ namespace Niobium.Messaging.ServiceBus
             }
             return sb.ToString();
         }
+
+        public static string ToSnakeCaseKeepCommonAbbreviations(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            var sb = new StringBuilder(text.Length * 2);
+            sb.Append(char.ToUpperInvariant(text[0]));
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                char current = text[i];
+                char previous = text[i - 1];
+                char? next = i + 1 < text.Length ? text[i + 1] : (char?)null;
+
+                bool isUpper = char.IsUpper(current);
+                bool prevIsUpper = char.IsUpper(previous);
+                bool prevIsLower = char.IsLower(previous);
+                bool nextIsLower = next.HasValue && char.IsLower(next.Value);
+
+                if (isUpper && (prevIsLower || (prevIsUpper && nextIsLower)))
+                {
+                    sb.Append('_');
+                }
+
+                sb.Append(char.ToUpperInvariant(current));
+            }
+
+            return sb.ToString();
+        }
     }
 }
